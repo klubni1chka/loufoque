6d134630d6e39627c188b6dc90bde912f158f0a1
klubni1chka <97652254+klubni1chka@users.noreply.github.com>
2022-05-03 19:31:43 +0600

    with cards

--- assignment2/routes/models/app.js
+++ assignment2/routes/models/app.js
@@ -1,3 +1,15 @@
+// // Define schema
+// import mongoose from "mongoose";
+//
+// const Schema = mongoose.Schema;
+//
+// const SomeModelSchema = new Schema({
+//     a_string: String,
+//     a_date: Date
+// });
+//
+// // Compile model from schema
+// const SomeModel = mongoose.model('SomeModel', SomeModelSchema);
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://Assel:<password>@cluster0.sefax.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -5,5 +17,5 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 client.connect(err => {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
-    client.close();
+    client.close().then(() =>console.log() );
 });
